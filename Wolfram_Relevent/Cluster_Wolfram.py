62380d98660d2f4ce199f17dfe5a0ab4c5b1471c
Amar1729 <amartiger20@gmail.com>
2015-11-08 01:40:54 -0500

    Wolfram API supports only float/int, no UTC timestamps

--- Wolfram_Relevent/Cluster_Wolfram.py
+++ Wolfram_Relevent/Cluster_Wolfram.py
@@ -5,11 +5,34 @@
 # This script parses the JSON, feeds it to fetcher.py, then returns the sorted array (JSON?) back to Javascript (???)
 
 import json
+from fetcher import fc
 
-# Given from Javascript:	[["col1","col2","col3"],["x1","y1","z1"],["x2","y2","z2"], ... ]
-# Format of call to Wolfram:	FindClusters[ (1,-8,2),(2,4,1) (100,-10,1) (50,20,3) (3,2,31) (5,1,8) (60,0,13) (0,3,250 }]
+# Given from Javascript:	e.g. [["Record ID","10 Minute Wind Gust","Heat Index"],["266257","16","69.0"],["266259","15","69.0"]]
+# Format of call to Wolfram:	FindClusters[ (1,-8,2), (2,4,1), (100,-10,1), (50,20,3), (3,2,31), (5,1,8), (60,0,13), (0,3,250) ]
+### NOOO
+# it's like this:
+#	FindClusters[{ {1,-8,2}, {2,4,1}, {100,-10,1}, {50,20,3} }]
+# Format of return from API:	{(1 | -8 | 2 2 | 4 | 1 5 | 1 | 8), (100 | -10 | 1 50 | 20 | 3 60 | 0 | 13), (3 | 2 | 31 0 | 3 | 250)}
 # Give back to Javascript:	Also JSON?
 
 def get(jstr):
-	parsed_string = json.loads(jstr)
-	
+	parsed = json.loads(jstr)
+	parsed.pop(0)
+
+	p = []
+	q = [0.0]*3
+
+	for row in parsed:
+		for idx, e in enumerate(row):
+			q[idx] = float(str(e))
+		p.append( (q[0], q[1], q[2]) )
+
+	ret = "[{"
+	for i in p:
+		ret = ret + str(i) + ","
+	ret = "FindClusters" + ret[:len(ret)-1] + "}]"
+
+	# not really
+	# return ret	# correct !
+
+	return str( fc(ret) )	
--- Wolfram_Relevent/fetcher.py
+++ Wolfram_Relevent/fetcher.py
@@ -2,29 +2,50 @@
 # -*- coding: utf-8 -*-
 import wap
 
-server = 'http://api.wolframalpha.com/v2/query.jsp'
-with open('../APPID') as f:
-	appid = f.readline().strip()
-input = str(raw_input())
+# IMPORTANT
+# This file was originally written by wolfram (not us)
+# Some example files were included to show how to access Wolfram API,
+#	and this is a heavily edited example file.
 
-waeo = wap.WolframAlphaEngine(appid, server)
+# Edits that need to be made:
+#	Currently file takes str input
+#		-> should be made a function
+#	Outputs Pod.title, img.src, img.alt for input and result
+#		-> should return img.alt for result
 
-queryStr = waeo.CreateQuery(input)
-wap.WolframAlphaQuery(queryStr, appid)
-result = waeo.PerformQuery(queryStr)
-result = wap.WolframAlphaQueryResult(result)
+# Find clusters
+def fc(data):
+	input = data
 
-for pod in result.Pods():
-	waPod = wap.Pod(pod)
-	title = "Pod.title: " + waPod.Title()[0]
-	print title
-	for subpod in waPod.Subpods():
-		waSubpod = wap.Subpod(subpod)
-		plaintext = waSubpod.Plaintext()[0]
-		img = waSubpod.Img()
-		src = wap.scanbranches(img[0], 'src')[0]
-		alt = wap.scanbranches(img[0], 'alt')[0]
-		print "-------------"
-		print "img.src: " + src
-		print "img.alt: " + alt
-	print "\n"
+	server = 'http://api.wolframalpha.com/v2/query.jsp'
+	with open('../../APPID') as f:
+		appid = f.readline().strip()
+
+	# Need to further parse data; maybe?
+
+	waeo = wap.WolframAlphaEngine(appid, server)
+
+	queryStr = waeo.CreateQuery(input)
+	wap.WolframAlphaQuery(queryStr, appid)
+	result = waeo.PerformQuery(queryStr)
+	result = wap.WolframAlphaQueryResult(result)
+
+	for pod in result.Pods():
+		waPod = wap.Pod(pod)
+		title = "Pod.title: " + waPod.Title()[0]
+		#print title
+		for subpod in waPod.Subpods():
+			waSubpod = wap.Subpod(subpod)
+			plaintext = waSubpod.Plaintext()[0]
+			img = waSubpod.Img()
+			src = wap.scanbranches(img[0], 'src')[0]
+			alt = wap.scanbranches(img[0], 'alt')[0]
+			#print "-------------"
+			#print "img.src: " + src
+			#print "img.alt: " + alt
+			if ( title.strip() == "Pod.title: Result" ):
+				return alt;
+		#print "\n"
+
+	# Never got a result :(
+	return 0;
