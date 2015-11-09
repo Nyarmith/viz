5ee7e50335832ae77261fb0cf8c38f00392b0fff
Amar1729 <amartiger20@gmail.com>
2015-11-09 00:59:54 -0500

    Parametrized gui

--- wolfram-scripts/fetcher.py
+++ wolfram-scripts/fetcher.py
@@ -14,7 +14,7 @@ import json
 ##
 ##	Input:	Complete Wolfram query ~ FindClusters[{ {x1,y1,z1}, {x2,y2,z2} ... }]
 ##	Output: Set of arrays sorted into clusters
-def FC(data):
+def FC(data, show_gui=False):
 	input = data
 
 	server = 'http://api.wolframalpha.com/v2/query.jsp'
@@ -33,19 +33,22 @@ def FC(data):
 	for pod in result.Pods():
 		waPod = wap.Pod(pod)
 		title = "Pod.title: " + waPod.Title()[0]
-		#print title
+		if(show_gui):
+			print title
 		for subpod in waPod.Subpods():
 			waSubpod = wap.Subpod(subpod)
 			plaintext = waSubpod.Plaintext()[0]
 			img = waSubpod.Img()
 			src = wap.scanbranches(img[0], 'src')[0]
 			alt = wap.scanbranches(img[0], 'alt')[0]
-			#print "-------------"
-			#print "img.src: " + src
-			#print "img.alt: " + alt
+			if(show_gui):
+				print "-------------"
+				print "img.src: " + src
+				print "img.alt: " + alt
 			if ( title.strip() == "Pod.title: Result" ):
 				return alt;
-		#print "\n"
+		if(show_gui):
+			print "\n"
 
 	# Never got a result :(
 	return 0;
@@ -68,7 +71,8 @@ def FC(data):
 ## Format of return from API:	{(1 | -8 | 2 2 | 4 | 1 5 | 1 | 8), (100 | -10 | 1 50 | 20 | 3 60 | 0 | 13), (3 | 2 | 31 0 | 3 | 250)}
 #		Different dependent on data type: ({266257., 16., 69.} | {266259., 15., 69.})
 ## Give back to visualizer:		Array of arrays
-def Interface(jstr):
+def main(jstr):
+	#jstr = raw_input()
 	parsed = json.loads(jstr)
 	parsed.pop(0)
 
@@ -145,14 +149,6 @@ def parse(Res):
 	m = 0 		# number of distinct matrices in result
 	lst = [[[]]]
 
-"""
-{(1 | -8 | 2 2 | 4 | 1 5 | 1 | 8), (100 | -10 | 1 50 | 20 | 3 60 | 0 | 13), (3 | 2 | 31 0 | 3 | 250)}
-({1 , -8 , 2 2 , 4 , 1 5 , 1 , 8}| {100 , -10 , 1 50 , 20 , 3 60 , 0 , 13}| {3 , 2 , 31 0 , 3 , 250})
-
-
-({266257., 16., 69.} | {266259., 15., 69.})
-"""
-
 	counter = 0
 	check = False
 	if(Res[0] == '{'):
@@ -171,7 +167,7 @@ def parse(Res):
 				counter = counter + 1
 			elif(Res[c1] == ','):
 				Res = stri[:c1] + "|" + stri[c1:]
-			elif(Res[c1] == ' ' and !check and counter==2):
+			elif(Res[c1] == ' ' and check==False and counter==2):
 				check = True
 			elif(Res[c1] == ' ' and check):
 				check = False
@@ -314,3 +310,6 @@ def parse(Res):
 
 		lst.pop(m)
 		return lst
+
+if __name__ == "__main__":
+	main()
