fa3aedd205a6906fbc3c2689c068759b1cb6795d
nyarmith <powah.serge@gmail.com>
2015-11-20 16:58:20 -0500

    added some member funtions

--- js/data_visualizer.js
+++ js/data_visualizer.js
@@ -7,9 +7,84 @@
  * @nameSpace algo_handler
  */
 if (typeof algo_handler == "undefined" || !algo_handler) {
-    var algo_handler = {};
+    function algo_handler(){};
 }
-algo_handler;//object that receives a bunch of javascript functions
+
+//Sets data fields and such
+algo_handler.prototype.set_data(myEntries, myFields)
+{
+    this.entries    = myEntries;
+    this.fields     = myFields;
+}
+
+//returns copy of object, if I wanted to implement more efficiently, I'd have end case in mind
+//and could return object that acts on original data in terms of indices
+algo_handler.prototype.get_subset(filter, myFields)
+{
+    var subset = [];
+    for (var i=0; i < this.entries.length; i++)
+    {
+        var bool = true;
+        for (var j=0; j < myFields.lenght; j++)
+        {
+            bool = bool && filter(this.entries[i][myFields[j]]);
+        }
+
+        if (bool)
+            subset.push(this.entries[i]);
+    }
+
+    new_set = algo_handler();   //create and returns a new algo handler
+    new_set.set_data(subset, this.fields);
+
+    return new_set;
+}
+
+
+algo_handler.prototype.get_mean(filter, myFields)
+{
+    var subset = [];
+    for (var i=0; i < this.entries.length; i++)
+    {
+        var bool = true;
+        for (var j=0; j < myFields.lenght; j++)
+        {
+            bool = bool && filter(this.entries[i][myFields[j]]);
+        }
+
+        if (bool)
+            subset.push(this.entries[i]);
+    }
+
+    new_set = algo_handler();   //create and returns a new algo handler
+    new_set.set_data(subset, this.fields);
+
+    return new_set;
+}
+
+algo_handler.prototype.get_mean(filter, myFields)
+{
+    var subset = [];
+    for (var i=0; i < this.entries.length; i++)
+    {
+        var bool = true;
+        for (var j=0; j < myFields.lenght; j++)
+        {
+            bool = bool && filter(this.entries[i][myFields[j]]);
+        }
+
+        if (bool)
+            subset.push(this.entries[i]);
+    }
+
+    new_set = algo_handler();   //create and returns a new algo handler
+    new_set.set_data(subset, this.fields);
+
+    return new_set;
+}
+
+
+//object that receives a bunch of javascript functions
 
 /**
  * data_handler root namespace.
